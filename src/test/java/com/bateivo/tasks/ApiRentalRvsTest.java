affbcfbf09ea8dad96539aad0f9e6cd4c0ad2c3a
Ivo K <bateivo>
2022-06-23 10:13:19 +0000

    Initial commit

---
+++ src/test/java/com/bateivo/tasks/ApiRentalRvsTest.java
@@ -0,0 +1,21 @@
+package com.bateivo.tasks;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class ApiRentalRvsTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
